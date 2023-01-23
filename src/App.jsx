4bf2de02642f7ef779045d7c8e9cab2ab128a4c7
stepanenkovladyslav <stepanenkovladyslav@gmail.com>
2023-01-23 08:40:02 +0200

    Removed useless comments and console.logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,24 @@
 import "./App.css";
 import React from "react";
-import {Routes,Route} from "react-router-dom"
-import Layout from "./components/Layout"
-import BlogpostsPage from "./pages/BlogpostsPage"
-import InformationPage from "./pages/InformationPage"
-import ReviewsPage from "./pages/ReviewsPage"
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import BlogpostsPage from "./pages/BlogpostsPage";
+import InformationPage from "./pages/InformationPage";
+import ReviewsPage from "./pages/ReviewsPage";
 import PostPage from "./pages/PostPage/PostPage";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
 
 function App() {
-  return (
-      <Routes>
-        <Route path='/' element={<Layout/>}>
-            <Route index element={<BlogpostsPage/>}/>
-            <Route path="post/:id" element={<PostPage/>}/>
-            <Route path='info' element={<InformationPage/>}/>
-            <Route path='reviews' element={<ReviewsPage/>}/>
-            <Route path='*' element={<ErrorPage/>}/>
-        </Route>
-      </Routes>
-  )
+	return (
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<BlogpostsPage />} />
+				<Route path="post/:id" element={<PostPage />} />
+				<Route path="info" element={<InformationPage />} />
+				<Route path="reviews" element={<ReviewsPage />} />
+				<Route path="*" element={<ErrorPage />} />
+			</Route>
+		</Routes>
+	);
 }
 export default App;
-
-
-/*
-
-hooks + useMemo
-
-3 - Начать попытки реализации модального окна
-4 - Реализовать личный хук для пагинации
-5 - Реализовать пагинацию постов и комметариев
-6 - Сделать сокращения в пагинации вместе с кнопками перехода на слудующую страницу
-7 - Если нет постов - удалили все со странички уведомить об этом 
-*/
-
-
-
--- src/components/PostCommentList.jsx
+++ src/components/PostCommentList.jsx
@@ -1,41 +1,60 @@
-import React from 'react'
-import PostComment from './UI/PostComment/PostComment'
-import style from '../pages/PostPage/PostPage.module.css'
-import Pagelist from './Pagelist'
-import CommentSection from './CommentSection'
+import React from "react";
+import PostComment from "./UI/PostComment/PostComment";
+import style from "../pages/PostPage/PostPage.module.css";
+import Pagelist from "./Pagelist";
+import CommentSection from "./CommentSection";
 
-  function getComments (comments, setComments, numberofpages, changePage, commentPage) {
-    console.log(setComments)
-      return (
-        <>
-        <CommentSection comments={comments} setComments={setComments}/>
-        {comments.length > 0 ? 
-        <>
-        <h2 className={style.commentHeading}>Comment Section</h2> 
-          <ul className={style.commentList}>{
-            comments.map(comment => {
-              return <PostComment title={comment.name} email={comment.email} body={comment.body} key={comment.id}/>
-            })
-          } </ul>
-          <Pagelist numberofpages={numberofpages} changePage={changePage} page={commentPage}/>
-          </>
-          :
-          <>
-          <h2 className={style.commentHeading}>Comment Section</h2> 
-          <h2 className={style.noComments}>There are no comments yet</h2>
-          </>
-          }
-
-      </>
-      )
-  }
-
-
-
-const PostCommentList = ({comments, setComments, numberofpages, changePage, page}) => {
-  return (
-      getComments(comments, setComments, numberofpages, changePage, page) 
-  )
+function getComments(
+	comments,
+	setComments,
+	numberofpages,
+	changePage,
+	commentPage
+) {
+	return (
+		<>
+			<CommentSection comments={comments} setComments={setComments} />
+			{comments.length > 0 ? (
+				<>
+					<h2 className={style.commentHeading}>Comment Section</h2>
+					<ul className={style.commentList}>
+						{comments.map((comment) => {
+							return (
+								<PostComment
+									title={comment.name}
+									email={comment.email}
+									body={comment.body}
+									key={comment.id}
+								/>
+							);
+						})}{" "}
+					</ul>
+					<Pagelist
+						numberofpages={numberofpages}
+						changePage={changePage}
+						page={commentPage}
+					/>
+				</>
+			) : (
+				<>
+					<h2 className={style.commentHeading}>Comment Section</h2>
+					<h2 className={style.noComments}>
+						There are no comments yet
+					</h2>
+				</>
+			)}
+		</>
+	);
 }
 
-export default PostCommentList
+const PostCommentList = ({
+	comments,
+	setComments,
+	numberofpages,
+	changePage,
+	page,
+}) => {
+	return getComments(comments, setComments, numberofpages, changePage, page);
+};
+
+export default PostCommentList;
